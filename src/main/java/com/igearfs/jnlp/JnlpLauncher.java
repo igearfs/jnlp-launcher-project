27ad79caf1fbbe271cb3ba7918965300799165c6
shadowgarden <jaknox@igearfs.com>
2025-04-11 17:31:03 -0500

    Copyright added in. Changed logging to be more system dependant rolling for windows in appdata .config for linux/osx

--- src/main/java/com/igearfs/jnlp/JnlpLauncher.java
+++ src/main/java/com/igearfs/jnlp/JnlpLauncher.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 In-Game Event, A Red Flag Syndicate LLC.
+ * All rights reserved.
+ *
+ */
+
 package com.igearfs.jnlp;
 
 import com.igearfs.jnlp.model.LaunchEntry;
--- src/main/java/com/igearfs/jnlp/Main.java
+++ src/main/java/com/igearfs/jnlp/Main.java
@@ -1,11 +1,53 @@
+/*
+ * Copyright (c) 2025 In-Game Event, A Red Flag Syndicate LLC.
+ * All rights reserved.
+ *
+ */
+
 package com.igearfs.jnlp;
 
+import com.igearfs.jnlp.util.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    // Static block to set up logging and system properties before main() runs
+    static {
+        String os = System.getProperty("os.name").toLowerCase();
+        String logPath;
+        String dataDir;
+
+        // Set log directory path based on OS
+        if (os.contains("win")) {
+            String appData = System.getProperty("user.home"); // Points to AppData\Roaming
+            logPath = appData + "/AppData/Roaming/SyncSyndicate/logs";
+        } else {
+            String userHome = System.getProperty("user.home");
+            logPath = userHome + "/.config/SyncSyndicate/logs"; // Or ~/.SyncSyndicate/data
+        }
+
+        // Set data directory path based on OS
+        if (os.contains("win")) {
+            String appData = System.getProperty("user.home"); // Points to AppData\Roaming
+            dataDir = appData + "/AppData/Roaming/SyncSyndicate/data";
+        } else {
+            String userHome = System.getProperty("user.home");
+            dataDir = userHome + "/.config/SyncSyndicate/data"; // Or ~/.SyncSyndicate/data
+        }
+
+        // Set system properties before logging setup
+        System.setProperty("os.logdir", logPath);
+        System.setProperty("os.dataDir", dataDir);
+
+
+
+    }
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        // Log paths to verify they are correctly set
+        LogManager.logInfo(logger, "Setting logDir: " +  System.getProperty("os.logdir"));
+        LogManager.logInfo(logger, "Setting dataDir: " + System.getProperty("os.dataDir"));
+
         JnlpLauncherApp.main(args);
     }
 }
